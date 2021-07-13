fa02d60ab2f5ee77483571f743abc8ce9ce38e74
Crolette <73672696+crolette@users.noreply.github.com>
2021-07-13 22:03:03 +0200

    app

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ window.onload = callApi()
 
 // Geo Ipify
 function callApi() {
-  fetch("https://geo.ipify.org/api/v1?apiKey=at_OWmOvTyJtp6NaX4cM8xtz2J0Zt7Mv")
+  fetch("https://geo.ipify.org/api/v1?apiKey=")
     .then((reponse) => reponse.json())
     .then((data) => {
       console.log(data);
@@ -53,41 +53,3 @@ function addMap(data) {
 
   L.marker([data.location.lat, data.location.lng], {icon: myIcon}).addTo(mymap);
 }
-
-
-// url GET : https://geo.ipify.org/api/v1?apiKey=at_OWmOvTyJtp6NaX4cM8xtz2J0Zt7Mv
-// {
-//     "ip": "8.8.8.8",
-//     "location": {
-//         "country": "US",
-//         "region": "California",
-//         "city": "Mountain View",
-//         "lat": 37.40599,
-//         "lng": -122.078514,
-//         "postalCode": "94043",
-//         "timezone": "-07:00",
-//         "geonameId": 5375481
-//     },
-//     "domains": [
-//         "0d2.net",
-//         "003725.com",
-//         "0f6.b0094c.cn",
-//         "007515.com",
-//         "0guhi.jocose.cn"
-//     ],
-//     "as": {
-//         "asn": 15169,
-//         "name": "Google LLC",
-//         "route": "8.8.8.0/24",
-//         "domain": "https://about.google/intl/en/",
-//         "type": "Content"
-//     },
-//     "isp": "Google LLC",
-//     "proxy": {
-//         "proxy": false,
-//         "vpn": false,
-//         "tor": false
-//     },
-// }
-
-//
